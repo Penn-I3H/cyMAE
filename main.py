4f5b744bc52228788605c1481dc8cd2d6c90050f
ukjung24 <chang-uk.jeong@pennmedicine.upenn.edu>
2024-11-19 15:53:17 -0500

    add log & output visual+raw

--- main.py
+++ main.py
@@ -16,6 +16,8 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import multiprocessing
 
+print("starting program")
+
 # Global variables for the model and device
 model = None
 args = None
@@ -36,12 +38,17 @@ def init_worker():
     global device
 
     device = "cpu"
+    print("loading model")
     checkpoint = torch.load(
         "model/cymae_30D_6L_pretrained0.25R_fold0_0.0064lr_200epoch_checkpoint-best.pth",
         map_location=torch.device(device),
         weights_only=False
     )
+    print("model loaded")
+    
     args = checkpoint['args']
+    
+    print("create model")
     model = create_model(
         args.model,
         pretrained=False,
@@ -53,9 +60,13 @@ def init_worker():
         use_mean_pooling=args.use_mean_pooling,
         init_scale=args.init_scale,
     ).to(device)
+    print("model created")
 
+    print("loading model state")
     model.load_state_dict(checkpoint['model'])
+    print("model state loaded")
     del checkpoint
+    
     model.eval()
 
 # Mapping from indices to class names
@@ -151,39 +162,52 @@ marker_list = [
 def process_file(input_path):
     global model
     global device
-
+    print("start processing")
     try:
+        print("file reading")
         _, input_data = read_fcs(input_path)
+        print("file read")
 
         missing_markers = [marker for marker in marker_list if marker not in input_data.columns]
         if missing_markers:
+            print("missing markers")
             raise ValueError(f"The FCS file must contain all the markers in {marker_list}.\n\nMissing markers: {', '.join(missing_markers)}")
         else:
+            print("data processing")
             input_data = input_data[marker_list].values
             input_data = torch.tensor(np.arcsinh(input_data)).to(device)  # torch.tensor (C, 30)
 
             batch_size = 1024
             preds = []
+            
+            print("running model")
             with torch.no_grad():
                 for i in range(0, input_data.size(0), batch_size):
                     batch_data = input_data[i:i + batch_size]
                     batch_preds = model(batch_data)
                     batch_preds = torch.max(batch_preds, 1)[1]
                     preds.extend([idx_to_class[idx.item()] for idx in batch_preds])
+            print("model finished")
+            json.dump(preds, open(OUTPUT_DIR + '/' + os.path.basename(input_path).replace(".fcs", "/CyMAE_raw_output.json"), 'w'))
 
             # Fit UMAP on the input data
+            print("fitting UMAP")
             reducer = umap.UMAP()
             embedding = reducer.fit_transform(input_data.cpu().numpy())
+            print("UMAP fitted")
 
             # Only color cell types of interest
             # Create a color palette
+            print("creating color palette for interested cells")
             unique_types, counts = np.unique(preds, return_counts=True)
             unique_classes = list(unique_types)
             palette = sns.color_palette("tab20", len(cell_types_of_interest))
             class_to_color = {cls: palette[i % len(palette)] for i, cls in enumerate(cell_types_of_interest)}
             default_color = (0.8, 0.8, 0.8)  # Gray color for other cell types
+            print("color palette created")
 
             # Plot UMAP embedding with colors based on interested categories
+            print("plotting UMAP for interested cells")
             plt.figure(figsize=(12, 10))
             for cls in unique_classes:
                 indices = [i for i, pred in enumerate(preds) if pred == cls]
@@ -197,22 +221,30 @@ def process_file(input_path):
             plt.xlabel("UMAP1", fontsize=15)
             plt.ylabel("UMAP2", fontsize=15)
             plt.tight_layout()
+            print("UMAP plotted")
 
             # Save the plot as a PNG file
-            output_image_path = OUTPUT_DIR + '/CyMAE_' + os.path.basename(input_path).replace(".fcs", "_(interest_cells).png")
+            print("saving image")
+            output_image_path = OUTPUT_DIR + '/' + os.path.basename(input_path).replace(".fcs", "/CyMAE_(interest_cells).png")
             plt.savefig(output_image_path, dpi=300)
             plt.close()
+            print("image saved")
 
             # Create plots of broad categories
             # Map detailed cell types to broader categories
+            print("mapping broad categories")
             broad_preds = [cell_type_mapping.get(p, 'Others') for p in preds]
+            print("broad categories mapped")
 
             # Create a color palette
+            print("creating color palette for broad categories")
             unique_broad_classes = list(set(broad_preds))
             palette = sns.color_palette("tab20", len(unique_broad_classes))
             class_to_color = {cls: palette[i % len(palette)] for i, cls in enumerate(unique_broad_classes)}
+            print("color palette created")
 
             # Plot UMAP embedding with colors based on broad categories
+            print("plotting UMAP for broad categories")
             plt.figure(figsize=(12, 10))
             for cls in unique_broad_classes:
                 indices = [i for i, pred in enumerate(broad_preds) if pred == cls]
@@ -223,11 +255,14 @@ def process_file(input_path):
             plt.xlabel("UMAP1", fontsize=15)
             plt.ylabel("UMAP2", fontsize=15)
             plt.tight_layout()
+            print("UMAP plotted")
 
             # Save the plot as a PNG file
-            output_image_path = OUTPUT_DIR + '/CyMAE_' + os.path.basename(input_path).replace(".fcs", "_(broad_types).png")
+            print("saving image")
+            output_image_path = OUTPUT_DIR + '/' + os.path.basename(input_path).replace(".fcs", "/CyMAE_(broad_types).png")
             plt.savefig(output_image_path, dpi=300)
             plt.close()
+            print("image saved")
 
             print(f"Processing of file {input_path} completed.")
     except Exception as e:
@@ -239,6 +274,8 @@ if __name__ == '__main__':
         os.makedirs(OUTPUT_DIR)
 
     fcs_files = glob.glob(f"{INPUT_DIR}/*.fcs")
+    for file in fcs_files:
+        os.makedirs(OUTPUT_DIR + f'/{os.path.basename(file).replace(".fcs", "")}', exist_ok=True)
 
     # Create a pool of worker processes
     with multiprocessing.Pool(processes=NUM_PROCESSES, initializer=init_worker) as pool:
