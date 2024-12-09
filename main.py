b1d07947eb560b694a3fa59616def10093779d64
ukjung24 <chang-uk.jeong@pennmedicine.upenn.edu>
2024-12-09 13:57:57 -0500

    Optimize number of parallel processes

--- main.py
+++ main.py
@@ -15,13 +15,22 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import pandas as pd
 import multiprocessing
+import logging
+from sklearn.decomposition import PCA
 
-print("starting program")
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    filename='out.log',
+    filemode='w'
+)
+
+logging.info("starting program")
 
 # Global variables for the model and device
 model = None
 args = None
-device = 'cpu'
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 INPUT_DIR = os.getenv('INPUT_DIR', './data/input')
 OUTPUT_DIR = os.getenv('OUTPUT_DIR', './data/output')
@@ -37,18 +46,17 @@ def init_worker():
     global args
     global device
 
-    device = "cpu"
-    print("loading model")
+    logging.info("loading model")
     checkpoint = torch.load(
         "model/cymae_30D_6L_pretrained0.25R_fold0_0.0064lr_200epoch_checkpoint-best.pth",
-        map_location=torch.device(device),
+        map_location=device,
         weights_only=False
     )
-    print("model loaded")
+    logging.info("model loaded")
     
     args = checkpoint['args']
     
-    print("create model")
+    logging.info("create model")
     model = create_model(
         args.model,
         pretrained=False,
@@ -60,11 +68,11 @@ def init_worker():
         use_mean_pooling=args.use_mean_pooling,
         init_scale=args.init_scale,
     ).to(device)
-    print("model created")
+    logging.info("model created")
 
-    print("loading model state")
+    logging.info("loading model state")
     model.load_state_dict(checkpoint['model'])
-    print("model state loaded")
+    logging.info("model state loaded")
     del checkpoint
     
     model.eval()
@@ -166,52 +174,56 @@ marker_list = [
 def process_file(input_path):
     global model
     global device
-    print("start processing")
+    logging.info("start processing")
     try:
-        print("file reading")
+        logging.info(f"file reading: {input_path}")
         _, input_data = read_fcs(input_path)
-        print("file read")
+        logging.info("file read")
 
         missing_markers = [marker for marker in marker_list if marker not in input_data.columns]
         if missing_markers:
-            print("missing markers")
+            logging.error("missing markers")
             raise ValueError(f"The FCS file must contain all the markers in {marker_list}.\n\nMissing markers: {', '.join(missing_markers)}")
         else:
-            print("data processing")
+            logging.info("data processing")
             input_data = input_data[marker_list].values
-            input_data = torch.tensor(np.arcsinh(input_data)).to(device)  # torch.tensor (C, 30)
+            input_data = torch.asinh(torch.tensor(input_data)).to(device)  # Use torch.asinh for efficiency
 
             batch_size = 1024
             preds = []
             
-            print("running model")
+            logging.info("running model")
             with torch.no_grad():
                 for i in range(0, input_data.size(0), batch_size):
                     batch_data = input_data[i:i + batch_size]
                     batch_preds = model(batch_data)
                     batch_preds = torch.max(batch_preds, 1)[1]
                     preds.extend([idx_to_class[idx.item()] for idx in batch_preds])
-            print("model finished")
-            json.dump(preds, open(OUTPUT_DIR + '/' + os.path.basename(input_path).replace(".fcs", "/CyMAE_raw_output.json"), 'w'))
+            logging.info("model finished")
 
-            # Fit UMAP on the input data
-            print("fitting UMAP")
-            reducer = umap.UMAP()
-            embedding = reducer.fit_transform(input_data.cpu().numpy())
-            print("UMAP fitted")
+            json_path = os.path.join(OUTPUT_DIR, os.path.basename(input_path).replace(".fcs", ""), "CyMAE_raw_output.json")
+            with open(json_path, 'w') as f:
+                json.dump(preds, f)
+
+            # Fit UMAP on the reduced data
+            logging.info("fitting UMAP")
+            reducer = umap.UMAP(n_jobs=-1)
+            # embedding = reducer.fit_transform(reduced_data)
+            embedding = reducer.fit_transform(input_data)
+            logging.info("UMAP fitted")
 
             # Only color cell types of interest
             # Create a color palette
-            print("creating color palette for interested cells")
+            logging.info("creating color palette for interested cells")
             unique_types, counts = np.unique(preds, return_counts=True)
             unique_classes = list(unique_types)
             palette = sns.color_palette("tab20", len(cell_types_of_interest))
             class_to_color = {cls: palette[i % len(palette)] for i, cls in enumerate(cell_types_of_interest)}
             default_color = (0.8, 0.8, 0.8)  # Gray color for other cell types
-            print("color palette created")
+            logging.info("color palette created")
 
             # Plot UMAP embedding with colors based on interested categories
-            print("plotting UMAP for interested cells")
+            logging.info("plotting UMAP for interested cells")
             plt.figure(figsize=(12, 10))
             for cls in unique_classes:
                 indices = [i for i, pred in enumerate(preds) if pred == cls]
@@ -225,30 +237,30 @@ def process_file(input_path):
             plt.xlabel("UMAP1", fontsize=15)
             plt.ylabel("UMAP2", fontsize=15)
             plt.tight_layout()
-            print("UMAP plotted")
+            logging.info("UMAP plotted")
 
             # Save the plot as a PNG file
-            print("saving image")
+            logging.info("saving image for interested cells")
             output_image_path = OUTPUT_DIR + '/' + os.path.basename(input_path).replace(".fcs", "/CyMAE_(interest_cells).png")
             plt.savefig(output_image_path, dpi=300)
             plt.close()
-            print("image saved")
+            logging.info("image saved")
 
             # Create plots of broad categories
             # Map detailed cell types to broader categories
-            print("mapping broad categories")
+            logging.info("mapping broad categories")
             broad_preds = [cell_type_mapping.get(p, 'Others') for p in preds]
-            print("broad categories mapped")
+            logging.info("broad categories mapped")
 
-            # Create a color palette
-            print("creating color palette for broad categories")
+            # Create a color palette for broad categories
+            logging.info("creating color palette for broad categories")
             unique_broad_classes = list(set(broad_preds))
             palette = sns.color_palette("tab20", len(unique_broad_classes))
             class_to_color = {cls: palette[i % len(palette)] for i, cls in enumerate(unique_broad_classes)}
-            print("color palette created")
+            logging.info("color palette created")
 
             # Plot UMAP embedding with colors based on broad categories
-            print("plotting UMAP for broad categories")
+            logging.info("plotting UMAP for broad categories")
             plt.figure(figsize=(12, 10))
             for cls in unique_broad_classes:
                 indices = [i for i, pred in enumerate(broad_preds) if pred == cls]
@@ -259,18 +271,18 @@ def process_file(input_path):
             plt.xlabel("UMAP1", fontsize=15)
             plt.ylabel("UMAP2", fontsize=15)
             plt.tight_layout()
-            print("UMAP plotted")
+            logging.info("UMAP plotted")
 
             # Save the plot as a PNG file
-            print("saving image")
+            logging.info("saving image for broad categories")
             output_image_path = OUTPUT_DIR + '/' + os.path.basename(input_path).replace(".fcs", "/CyMAE_(broad_types).png")
             plt.savefig(output_image_path, dpi=300)
             plt.close()
-            print("image saved")
+            logging.info("image saved")
 
-            print(f"Processing of file {input_path} completed.")
+            logging.info(f"Processing of file {input_path} completed.")
     except Exception as e:
-        print(f"Error processing file {input_path}: {e}")
+        logging.error(f"Error processing file {input_path}: {e}")
 
 if __name__ == '__main__':
     # Ensure the output directory exists
@@ -279,8 +291,9 @@ if __name__ == '__main__':
 
     fcs_files = glob.glob(f"{INPUT_DIR}/*.fcs")
     for file in fcs_files:
-        os.makedirs(OUTPUT_DIR + f'/{os.path.basename(file).replace(".fcs", "")}', exist_ok=True)
-
+        os.makedirs(os.path.join(OUTPUT_DIR, os.path.basename(file).replace(".fcs", "")), exist_ok=True)
+    
+    num_worker = min(NUM_PROCESSES, len(fcs_files))
     # Create a pool of worker processes
-    with multiprocessing.Pool(processes=NUM_PROCESSES, initializer=init_worker) as pool:
+    with multiprocessing.Pool(processes=num_worker, initializer=init_worker) as pool:
         pool.map(process_file, fcs_files)
